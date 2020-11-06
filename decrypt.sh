d4c7faebffd79e3c6a40583bcc5bfaeb396c7173
momoterraw <andrewvmail@gmail.com>
2020-11-05 16:03:35 -0800

    Update decrypt.sh

--- decrypt.sh
+++ decrypt.sh
@@ -2,6 +2,8 @@
 dbName=$1
 key=$2
 
+brew install sqlcipher
+
 if [ -z "$key" ] ; then 
 	echo "Please provide a database and key"
 	exit 1
@@ -12,9 +14,22 @@ key=$(cat $key);
 dbName_plain="${dbName}_plain.db"
 dbName_enc="${dbName}.db"
 
+
 # remove existing plain database 
 rm -f "$dbName_plain"
 
 echo "Decrypting $dbName using key $key"
-echo "PRAGMA cipher_default_kdf_iter = 4000;PRAGMA key='$key';select count(*) from sqlite_master;ATTACH DATABASE '${dbName_plain}' AS plaintext KEY '';SELECT sqlcipher_export('plaintext');DETACH DATABASE plaintext;" | sqlcipher ${dbName_enc}
+
+sqlcipher $dbName_enc <<-EOF
+.timeout 2000
+PRAGMA cipher_default_kdf_iter = 1;
+PRAGMA key = '$key';
+PRAGMA kdf_iter = 1;
+PRAGMA cipher_memory_security = OFF;
+select count(*) from sqlite_master;
+ATTACH DATABASE '${dbName_plain}' AS plaintext KEY '';
+SELECT sqlcipher_export('plaintext');
+DETACH DATABASE plaintext;
+EOF
+
 echo "Done."
